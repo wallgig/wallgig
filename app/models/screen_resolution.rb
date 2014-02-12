28287617ea5e8ad530cc162f71dd453268861693
Jian <jianyuan@gmail.com>
2014-02-12 14:55:28 +0000

    Cache screen resolution for index

--- app/models/screen_resolution.rb
+++ app/models/screen_resolution.rb
@@ -14,6 +14,14 @@ class ScreenResolution < ActiveRecord::Base
 
   default_scope -> { order("case when category = 'widescreen' then 1 else 2 end ASC, width DESC, height DESC") }
 
+  def self.group_by_category_for_index
+    Rails.cache.fetch('screen_resolution_group_by_category_for_index', expires_in: 1.day) do
+      self.all.to_a
+              .map      { |sr| sr.serializable_hash(only: [:width, :height], methods: :category_text).with_indifferent_access }
+              .group_by { |sr| sr[:category_text] }
+    end
+  end
+
   def to_s
     "#{width}&times;#{height}".html_safe
   end
