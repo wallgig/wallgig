4224813cbf5da54619e21ce3f0f7198e7161d825
Jian <jianyuan@gmail.com>
2014-02-14 04:30:40 +0000

    Optimize category index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,13 +3,20 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.roots.alphabetically
-    @tags       = Tag.not_empty.order(wallpapers_count: :desc).page(params[:page])
+    @tags = Tag.includes(:category)
+               .not_empty
+               .order(wallpapers_count: :desc)
+               .page(params[:page])
   end
 
   def show
-    @ancestors  = @category.ancestors
+    @ancestors = @category.ancestors
     @categories = @category.children.alphabetically
-    @tags       = Tag.in_category(@category).not_empty.order(wallpapers_count: :desc).page(params[:page])
+    @tags = Tag.includes(:category)
+               .in_category(@category)
+               .not_empty
+               .order(wallpapers_count: :desc)
+               .page(params[:page])
 
     render action: 'index'
   end
