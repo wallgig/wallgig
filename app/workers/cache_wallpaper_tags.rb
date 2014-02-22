c67f3a4acf6aaa651ab4c9caf301e6604a34fc8d
Jian <jianyuan@gmail.com>
2014-02-22 14:38:43 +0000

    Fix worker

--- app/workers/cache_wallpaper_tags.rb
+++ app/workers/cache_wallpaper_tags.rb
@@ -1,11 +1,13 @@
 class CacheWallpaperTags
   include Sidekiq::Worker
 
-  def perform(wallpaper_id: nil, tag_id: nil)
-    Wallpaper.where(id: wallpaper_id).find_each(&:cache_tag_list) unless wallpaper_id.nil?
+  def perform(options = {})
+    if options[:wallpaper_id].present?
+      Wallpaper.where(id: options[:wallpaper_id]).find_each(&:cache_tag_list)
+    end
 
-    unless tag_id.nil?
-      Tag.where(id: tag_id).find_each do |tag|
+    if options[:tag_id].present?
+      Tag.where(id: options[:tag_id]).find_each do |tag|
         tag.wallpapers.find_each(&:cache_tag_list)
       end
     end
