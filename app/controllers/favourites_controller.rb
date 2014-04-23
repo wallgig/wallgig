7d9548426a7c962bf2a2e9c67d66f90e27b044b5
Jian <jianyuan@gmail.com>
2014-04-23 02:08:10 +0100

    Fix favourites index

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,7 +1,10 @@
 class FavouritesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_user
+  layout 'user_profile'
 
+  # GET /users/1/favourites
+  # GET /users/1/favourites.json
   def index
     @wallpapers = @user.favourite_wallpapers
                        .accessible_by(current_ability, :read)
@@ -10,17 +13,18 @@ class FavouritesController < ApplicationController
 
     @wallpapers = WallpapersDecorator.new(@wallpapers, context: { current_user: current_user })
 
-    if request.xhr?
-      render partial: 'wallpapers/list', layout: false, locals: { wallpapers: @wallpapers }
-    else
-      render layout: 'user_profile'
+    respond_to do |format|
+      format.html
+      format.json do
+        html = render_to_string partial: 'wallpapers/list', layout: false, locals: { wallpapers: @wallpapers }, formats: [:html]
+        render json: { html: html }
+      end
     end
   end
 
   private
-
-  def set_user
-    @user = User.find_by_username!(params[:user_id])
-    authorize! :read, @user
-  end
+    def set_user
+      @user = User.find_by_username!(params[:user_id])
+      authorize! :read, @user
+    end
 end
