3cac25a6a7a1d1b83afa30966bd74a215eae690e
Jian Yuan <jianyuan@gmail.com>
2014-07-31 23:22:41 +0100

    Better low res wallpaper

--- app/helpers/wallpapers_helper.rb
+++ app/helpers/wallpapers_helper.rb
@@ -1,2 +1,16 @@
 module WallpapersHelper
+  def image_tag_for_wallpaper_stage(wallpaper)
+    style = [
+      "background: url(#{wallpaper.image.thumb('320x320').url}) no-repeat center center fixed",
+      'background-size: cover'
+    ].join(';')
+
+    image_tag(
+      wallpaper.requested_image_url,
+      :width => wallpaper.requested_image_resolution.width,
+      :height => wallpaper.requested_image_resolution.height,
+      :class => "img-wallpaper img-#{wallpaper.format} state-1",
+      :style => style
+    )
+  end
 end
