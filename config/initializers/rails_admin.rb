cfbfe47f3258f1aabb3302f90db534ee09926611
Jian Yuan <jianyuan@gmail.com>
2014-08-13 01:23:22 +0100

    Disable bulk delete

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,10 +3,10 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
@@ -21,7 +21,7 @@ RailsAdmin.config do |config|
     index                         # mandatory
     new
     export
-    bulk_delete
+    # bulk_delete
     show
     edit
     delete
