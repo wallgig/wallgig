7473b13d6047a2a0d03f6ed7f9c2ee186a208d88
Jian Yuan <jianyuan@gmail.com>
2014-08-20 22:43:55 +0100

    Refactor `before_action`s

--- app/controllers/wallpapers_controller.rb
+++ app/controllers/wallpapers_controller.rb
@@ -1,9 +1,10 @@
 class WallpapersController < ApplicationController
   WALLPAPER_SEARCH_CACHE_TTL = 10.minutes
 
-  before_action :authenticate_user!, except: [:index, :show, :set_profile_cover, :toggle_favourite, :collections, :toggle_collect]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_user, only: [:index]
-  before_action :set_wallpaper, only: [:show, :edit, :update, :destroy, :update_purity, :history, :set_profile_cover, :toggle_favourite, :collections, :toggle_collect]
+  before_action :set_wallpaper,
+                except: [:index, :new, :create, :save_search_params]
   before_action :record_wallpaper_impression, only: :show
 
   include WallpaperSearchParams
