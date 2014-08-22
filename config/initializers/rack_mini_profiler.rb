d21f244d25b3a000e6ab020c67f8ddd3bad8bd2b
Jian Yuan <jianyuan@gmail.com>
2014-08-22 12:29:12 +0100

    Fix rack mini profiler

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,3 +1,4 @@
+Rack::MiniProfiler.config.storage_options = { host: '127.0.0.1', port: 6379 }
 Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 Rack::MiniProfiler.config.skip_paths = ['/admin']
 
