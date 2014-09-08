d57fdead9b8c2618d0935c6130112a2d26e6f34a
Jian Yuan <jianyuan@gmail.com>
2014-09-09 00:49:08 +0100

    Add unauthorized event

--- app/assets/javascripts/v2/application.js
+++ app/assets/javascripts/v2/application.js
@@ -6,8 +6,25 @@
 
 //= require_self
 
+/* global _, Vue, superagent */
+
 (function (exports) {
   exports.app = new Vue({
-    el: '#wallgig-app'
+    el: '#wallgig-app',
+
+    created: function () {
+      this.$on('unauthorized', this.showUnauthorizedMessage);
+    },
+
+    methods: {
+      showUnauthorizedMessage: function (message) {
+        // TODO use modal box
+        if (message) {
+          alert(message);
+        } else {
+          alert('You are not authorized to perform this action!');
+        }
+      }
+    }
   });
 })(window);
